9944b02036bbb0dc82c5061574f8d33053d7dc03
Alien663 <rite2844@gmail.com>
2023-01-10 10:39:52 +0800

    fix:modify delete competittion

--- Schema/dbo/Stored Procedures/xp_CompetitionDelete.sql
+++ Schema/dbo/Stored Procedures/xp_CompetitionDelete.sql
@@ -1,10 +1,11 @@
-﻿create     procedure [dbo].[xp_CompetitionDelete]
+﻿
+CREATE     procedure [dbo].[xp_CompetitionDelete]
 	@CID int
 as begin
 	if exists(select 1 from Competition where CCID = @CID)
 		raiserror('There is at least one sub competition need to delete', 18, 1)
-	if exists(select 1 from Participate where CID = @CID)
-		raiserror('There is at least one person still participate the competition', 18, 2)
+	if exists(select 1 from Team where CID = @CID)
+		raiserror('There is at least one team still participate the competition', 18, 2)
 	if exists(select 1 from Game where CID = @CID)
 		raiserror('There is at least one game under this competition', 18, 3)
 	delete CompetitionArchive where CID = @CID
